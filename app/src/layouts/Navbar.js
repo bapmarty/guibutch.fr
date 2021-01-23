7a9ff5827ee9f3162fe7039a429e8567765117d5
baptistemrt <contact@baptistemrt.fr>
2021-01-23 13:20:39 +0100

    :bug: Fix no reload of name button

--- app/src/layouts/Navbar.js
+++ app/src/layouts/Navbar.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Cookies from 'js-cookie';
 
@@ -5,6 +6,12 @@ import Logo from "../assets/images/white-guibutch.png";
 import "../assets/scss/layouts/Navbar.scss";
 
 const Navbar = () => {
+
+  const [game, setGame] = useState({route: "/create-game", name: "Créer une partie"});
+
+  useEffect(() => {
+    Cookies.get('game') ? setGame({route: "/game", name: "Partie en cours"}) : setGame({route: "/create-game", name: "Créer une partie"});
+  })
   return (
     <>
       <nav>
@@ -13,7 +20,7 @@ const Navbar = () => {
         </div>
         <div>
           <ul className="nav-list">
-            {Cookies.get('game') ? (<li><NavLink to="/game" className="outlined-button">Partie en cours</NavLink></li>) : (<li><NavLink to="/create-game" className="outlined-button">Créer une partie</NavLink></li>)}
+            <li><NavLink to={game.route} className="outlined-button">{game.name}</NavLink></li>
           </ul>
         </div>
       </nav>
