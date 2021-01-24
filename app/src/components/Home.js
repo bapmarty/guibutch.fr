932650c75a5653cba657128ca904d31a54a7f461
baptistemrt <contact@baptistemrt.fr>
2021-01-24 11:54:03 +0100

    :sparkles: Add homepage

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,7 +1,14 @@
+import PlainLogo from "../assets/images/logo.svg";
+
+import "../assets/scss/components/Home.scss";
+
 const Home = () => {
   return (
     <>
-      Home
+      <section className="home">
+        <img src={PlainLogo} alt={"logo"} />
+        <h1>Bienvenue sur le site officiel du guibutch</h1>
+      </section>
     </>
   )
 }
